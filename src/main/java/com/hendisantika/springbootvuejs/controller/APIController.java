883add7daf45500005a931589b38d83ed14d9b9a
Hendi Santika <hendisantika@gmail.com>
2017-12-06 21:49:05 +0700

    #13 - Add schema.sql

---
+++ src/main/java/com/hendisantika/springbootvuejs/controller/APIController.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootvuejs.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-vuejs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/12/17
+ * Time: 21.48
+ * To change this template use File | Settings | File Templates.
+ */
+public class APIController {
+}
