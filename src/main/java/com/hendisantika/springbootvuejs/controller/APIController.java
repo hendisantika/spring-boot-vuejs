ff43b4b4de987167dc9108b5bee46b98e2d41748
Hendi Santika <hendisantika@gmail.com>
2017-12-06 21:49:43 +0700

    #14 - Add Api Controller

--- src/main/java/com/hendisantika/springbootvuejs/controller/APIController.java
+++ src/main/java/com/hendisantika/springbootvuejs/controller/APIController.java
@@ -1,5 +1,15 @@
 package com.hendisantika.springbootvuejs.controller;
 
+import com.hendisantika.springbootvuejs.entity.Country;
+import com.hendisantika.springbootvuejs.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-vuejs
@@ -10,5 +20,16 @@ package com.hendisantika.springbootvuejs.controller;
  * Time: 21.48
  * To change this template use File | Settings | File Templates.
  */
+
+@RestController
+@RequestMapping("/api")
 public class APIController {
+    @Autowired
+    CountryRepository countryRepo;
+
+    @GetMapping("/countries")
+    public Page<Country> getCountries(Pageable pageable,
+                                      @RequestParam(name="search", required=false, defaultValue="") String search){
+        return countryRepo.findByNameContainingOrderByCode(search, pageable);
+    }
 }
